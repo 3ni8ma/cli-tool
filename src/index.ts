cdf1967de7deeb2780d0682502e1f315c1eb72e9
Aarush Karak <worldshaans@gmail.com>
2026-07-15 01:30:31 -0400

    Update cli-tool: 2026-07-15 02:30 [skip ci]

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-// Build: 2026-07-15 02:01:28 | run 1784095288
+// Build: 2026-07-15 02:30:52 | run 1784097052
 #!/usr/bin/env node
 import { Command } from "commander";
 import { existsSync, writeFileSync, mkdirSync, copyFileSync } from "fs";
